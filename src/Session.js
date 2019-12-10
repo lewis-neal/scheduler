624cdb807e12fdce86bf4fe0d6465dbdceeac64d
Lewis Neal <l_neal_@hotmail.co.uk>
2019-12-10 23:27:55 +0000

    Remove utilities module to make things work

--- src/Session.js
+++ src/Session.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import firebase from './Firebase';
 import Person from './Person';
-import { intersection } from './Utilities';
 
 class Session extends Component {
   constructor(props) {
@@ -57,6 +56,37 @@ class Session extends Component {
       redirect: true,
     });
   };
+
+  intersection() {
+    let result = [];
+    let lists;
+
+    if(arguments.length === 1) {
+      lists = arguments[0];
+    } else {
+      lists = arguments;
+    }
+
+    for(let i = 0; i < lists.length; i++) {
+      let currentList = lists[i];
+      for(let y = 0; y < currentList.length; y++) {
+        let currentValue = currentList[y];
+        if(result.indexOf(currentValue) === -1) {
+          let existsInAll = true;
+          for(let x = 0; x < lists.length; x++) {
+            if(lists[x].indexOf(currentValue) === -1) {
+              existsInAll = false;
+              break;
+            }
+          }
+          if(existsInAll) {
+            result.push(currentValue);
+          }
+        }
+      }
+    }
+    return result;
+  }
 
   render() {
     let id = typeof this.props.match.params.id !== 'undefined' ? this.props.match.params.id : '';
