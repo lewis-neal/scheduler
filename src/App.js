aae2d10a770f16cfdfaa92725095b7e148098d07
Lewis Neal <l_neal_@hotmail.co.uk>
2019-12-13 23:54:06 +0000

    Use HashRouter, add base url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './Homepage';
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL} >
           <div className="App-header">
             <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
             <h1>Scheduler</h1>
