e7ee92ad17807cb4eceaa7256160830227ef7b99
Lewis Neal <l_neal_@hotmail.co.uk>
2019-12-13 22:36:06 +0000

    Remove unneeded config from firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,9 @@ import 'firebase/database';
 
 var firebaseConfig = {
   apiKey: "AIzaSyAhN0IcVrgBKD5U6KBebY9sWM-_DpxQ6sQ",
-  authDomain: "scheduler-85cc7.firebaseapp.com",
   databaseURL: "https://scheduler-85cc7.firebaseio.com",
   projectId: "scheduler-85cc7",
-  storageBucket: "scheduler-85cc7.appspot.com",
-  messagingSenderId: "401148320864",
   appId: "1:401148320864:web:476dd010a9c0085d557705",
-  measurementId: "G-XRH7KEFFXV"
 };
 
 firebase.initializeApp(firebaseConfig);
