b8978e94250028efcf01adc08ff5a1bc785965ab
Lewis Neal <l_neal_@hotmail.co.uk>
2019-12-25 23:35:49 +0000

    Move intersection function into utilities module

--- src/Session.js
+++ src/Session.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 import firebase from './Firebase';
 import Person from './Person';
 import copy from 'copy-to-clipboard';
+import { intersection  } from './Utilities';
 
 function Session(props) {
   const [redirect, setRedirect] = useState(false);
@@ -11,37 +12,6 @@ function Session(props) {
   const dbRef = firebase.database().ref().child(
     'sessions/' + props.match.params.id + '/people'
   );
-
-  function intersection() {
-    let result = [];
-    let lists;
-
-    if(arguments.length === 1) {
-      lists = arguments[0];
-    } else {
-      lists = arguments;
-    }
-
-    for(let i = 0; i < lists.length; i++) {
-      let currentList = lists[i];
-      for(let y = 0; y < currentList.length; y++) {
-        let currentValue = currentList[y];
-        if(result.indexOf(currentValue) === -1) {
-          let existsInAll = true;
-          for(let x = 0; x < lists.length; x++) {
-            if(lists[x].indexOf(currentValue) === -1) {
-              existsInAll = false;
-              break;
-            }
-          }
-          if(existsInAll) {
-            result.push(currentValue);
-          }
-        }
-      }
-    }
-    return result;
-  }
 
   function getPeopleElement() {
     if (typeof people !== 'undefined' && Object.entries(people).length > 0) {
