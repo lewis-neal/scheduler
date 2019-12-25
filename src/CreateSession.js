cd7492e59e71b2ef9134a81898cf4f9053964031
Lewis Neal <l_neal_@hotmail.co.uk>
2019-12-25 20:58:16 +0000

    Use hooks syntax in CreateSession

--- src/CreateSession.js
+++ src/CreateSession.js
@@ -1,52 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import firebase from './Firebase';
 
-class CreateSession extends Component {
-  constructor() {
-    super();
-    this.dbRef = firebase.database().ref().child('sessions');
-    this.state = {
-      redirect: false,
-    };
-  }
+function CreateSession() {
+  const dbRef = firebase.database().ref().child('sessions');
+  const [redirect, setRedirect] = useState(false);
 
-  handleClick = () => {
-    this.setState({
-      redirect: true,
-    });
-
-    this.dbRef.once('value').then((result) => {
-      let data = result.val();
-      let cutOff = new Date();
+  function handleClick() {
+    setRedirect(true);
+    dbRef.once('value').then((result) => {
+      const data = result.val();
+      const cutOff = new Date();
       cutOff.setDate(cutOff.getDate() - 1);
       Object.keys(data).forEach((id) => {
         if (new Date(data[id].timestamp) < cutOff) {
-          this.dbRef.child(id).remove();
+          dbRef.child(id).remove();
         }
       });
     });
   }
 
-  render() {
-    if (this.state.redirect) {
-      const session = {
-        'people': '',
-        'timestamp': new Date().toJSON(),
-      };
-      let sessionId = this.dbRef.push(session).key;
-      let path = '/session/' + sessionId;
-      return (
-        <Redirect push to={path} />
-      );
-    }
-
+  if (redirect) {
+    const session = {
+      'people': '',
+      'timestamp': new Date().toJSON(),
+    };
+    const sessionId = dbRef.push(session).key;
+    const path = '/session/' + sessionId;
     return (
-      <div>
-        <button className="create-session-button" onClick={this.handleClick}>Create</button>
-      </div>
+      <Redirect push to={path} />
     );
   }
+
+  return (
+    <div>
+      <button className="create-session-button" onClick={handleClick}>Create</button>
+    </div>
+  );
 }
 
 export default CreateSession;
